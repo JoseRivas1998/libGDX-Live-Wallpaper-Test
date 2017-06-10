078d89dfe8617cf04457e7fdc76076d2ea330479
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2017-06-09 19:06:12 -0700

    Removed uneeded static class

--- android/src/com/tcg/libgdxwallpaper/TCGLiveWallpaper.java
+++ android/src/com/tcg/libgdxwallpaper/TCGLiveWallpaper.java
@@ -19,16 +19,4 @@ public class TCGLiveWallpaper extends AndroidLiveWallpaperService {
         initialize(new Game(), config);
 
     }
-
-    public static class TCGLiveWallpaperListener implements AndroidWallpaperListener {
-        @Override
-        public void offsetChange(float xOffset, float yOffset, float xOffsetStep, float yOffsetStep, int xPixelOffset, int yPixelOffset) {
-            Log.i("LiveWallpaper test", "offsetChange(xOffset:"+xOffset+" yOffset:"+yOffset+" xOffsetSteep:"+xOffsetStep+" yOffsetStep:"+yOffsetStep+" xPixelOffset:"+xPixelOffset+" yPixelOffset:"+yPixelOffset+")");
-        }
-
-        @Override
-        public void previewStateChange(boolean isPreview) {
-            Log.i("LiveWallpaper test", "previewStateChange(isPreview:"+isPreview+")");
-        }
-    }
 }
